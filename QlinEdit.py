f5982ee5475974c0f634e7449f3d370ebec98392
wx <869154901@qq.com>
2020-04-11 20:50:04 +0800

    QlinEdit回显模式和限制输入

--- QlinEdit.py
+++ QlinEdit.py
@@ -5,8 +5,14 @@ QLineEdit的回显模式(EchoMode)
 3.Password  密码模式
 4.PasswordEchoOnEdit
 '''
+'''
+QLineEdit的限制输入
+
+'''
 import sys
 from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QLabel, QFormLayout, QLineEdit
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QIntValidator, QDoubleValidator,QRegExpValidator
 
 
 class QLabelDemo(QWidget):
@@ -23,12 +29,29 @@ class QLabelDemo(QWidget):
         passwordEchoOnEdit = QLineEdit()
 
         # 表单布局
-        formLayout.addRow("Normal", normalEdit)
+        formLayout.addRow("Normal", normalEdit)     # 整型
         formLayout.addRow("NoEcho", noEchoEdit)
-        formLayout.addRow("password", passwordEdit)
+        formLayout.addRow("password", passwordEdit) # 浮点型
         formLayout.addRow("passwordEchoOnEdit", passwordEchoOnEdit)
 
         # setPlaceholderText()   输入提示
+
+        # 整数
+        intValidator = QIntValidator(self)
+        intValidator.setRange(1, 99)
+        # 浮点数,小数点2位
+        doubleValidator = QDoubleValidator(self)
+        doubleValidator.setRange(-360, 360)
+        doubleValidator.setNotation(QDoubleValidator.StandardNotation)  # 标准表示法
+        doubleValidator.setDecimals(2)
+        # 字符和数字
+        reg = QRegExp('[a-zA-Z0-9]+$')
+        validator = QRegExpValidator(self)
+        validator.setRegExp(reg)
+        # 校验器
+        normalEdit.setValidator(intValidator)
+        passwordEdit.setValidator(doubleValidator)
+        passwordEchoOnEdit.setValidator(validator)
 
         # setEchoMode() 设置模式
         normalEdit.setEchoMode(QLineEdit.Normal)

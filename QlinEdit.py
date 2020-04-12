218967313758f2a3bf71984049c98a711161d541
wx <869154901@qq.com>
2020-04-12 18:15:23 +0800

    QLabel

--- QlinEdit.py
+++ QlinEdit.py
@@ -7,7 +7,28 @@ QLineEdit的回显模式(EchoMode)
 '''
 '''
 QLineEdit的限制输入
-
+setValidator()
+QLineEdit的掩码限制输入
+setInputMask('')
+A ASCII字母字符是必须的，取值空间是A-Z,a-z
+a ASCII字母字符是允许的但不是必须的.
+N ASCII字母字符是必须的. A-Z, a-z, 0-9.
+n ASCII 字母字符是允许的但不是必须的.
+X 任何字符都可以，是必须需要的.
+x 任何字符都允许的，但不是必须需要的.
+9 ASCII 数字是必须要的. 0-9.
+0 ASCII 数字是允许的，但不是必须要的.
+D ASCII  数字是必须要的. 1-9.
+d ASCII 数字是允许的，但不是必须要的 (1-9).
+# ASCII 数字是或加减符号允许的，但不是必须要的.
+H 十六进制数据字符是必须要的. A-F, a-f, 0-9.
+h 十六进制数据字符是允许的，但不是必须要的.
+B 二进制数据字符是必须要的. 0-1.
+b 二进制数据字符是允许的，但不是必须要的.
+> 所有的字符字母都都大写的.
+< 所有的字符字幕都是小写的.
+! 关闭大小写.
+\ 使用 \ 去转义上面的字符，如果再需要显示上述字符的时候.
 '''
 import sys
 from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QLabel, QFormLayout, QLineEdit
@@ -58,6 +79,10 @@ class QLabelDemo(QWidget):
         noEchoEdit.setEchoMode(QLineEdit.NoEcho)
         passwordEdit.setEchoMode(QLineEdit.Password)
         passwordEchoOnEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+
+        # 使用掩码
+        # normalEdit.setInputMask('000.000.000.000;_') 以这个形式输入，;表示没有就是下划线
+        # normalEdit.setInputMask('>AAAAA-AAAAA-AAAAA;#')
 
         self.setLayout(formLayout)
         self.setWindowTitle('QLineEdit')

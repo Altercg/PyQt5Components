46f0e7781b2d9691b1649844c8d752619ea87c8f
wx <869154901@qq.com>
2020-04-14 21:16:06 +0800

    clipboard

--- drag.py
+++ drag.py
@@ -1,4 +1,27 @@
 '''
+剪贴板
+复制文本：
+    clipboard = QApplication.clipboard()
+    clipboard.setText('hello world')
+粘贴文本：
+    clipboard = QApplication.clipboard()
+    self.textlabel.setText(clipboard.text())
+复制图片：
+    clipboard = QApplication.clipboard()
+    clipboard.setPixmap('./bg.jpg')
+粘贴图片：
+    clipboard = QApplication.clipboard()
+    self.imagelabel.setPixmap(clipboard.pixmap())
+复制Html：
+    mimeData = QMimeData()
+    mimeData.setHtml('<a>test</a>>')
+    clipboard = QApplication.clipboard()
+    clipboard.setMimeData(mimeData)
+粘贴Html：
+    clipboard = QApplication.clipboard()
+    mimeData = clipboard.mimeData()
+    if mimeData.hasHtml():
+        self.textlabel.setText(mimeData.html())
 让控件支持拖拽动作
 A.setDragEnabled(True)
 B.setAcceptDrops(True)

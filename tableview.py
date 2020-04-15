0b7ec00d39ca570b08d6a2e122757ebca2a0844d
wx <869154901@qq.com>
2020-04-15 21:22:15 +0800

    update more and more

--- tableview.py
+++ tableview.py
@@ -9,6 +9,44 @@ QTableView的子类，扩展的表格控件（QTableWidget）
     tablewidget = QTableWidget()
     tablewidget.setRowCount(4)  # 设置行数
     tablewidget.setColumnCount(3)   # 设置列数
+
+    textItem = QTableWidgetItem('小明')
+    tableWidget.setItem(0,0,textItem)   # cell里面加文本
+    combox.setStyleSheet('QComboBox{margin:3px};')  # QSS，设置combox控件样式
+    tableWidget.setCellWidget(0,1,combox)   #cell里面加combox
+
+    findItems(text,QtCore.Qt.MatchExactly) # 数据定位，Qt.MatchStartsWith以此开头，Qt.MatchExactly精确定位，返回列表
+    item.setBackground(QBrush(QColor(0,255,0)))     # 设置背景色
+    item.setForeground(QBrush(QColor(255,0,0)))     # 设置字体色
+    newItem.setFont(QFont('Times',14,QFont.Black))  # 设置字体样式
+    tableWidget.verticalScrollBar().setSliderPosition(row)
+    # setSliderPosition(row)  # 找到满足的单元格，定位单元格所在的行
+    # verticalScrollBar()     #获得滚动条
+
+    sortItems(columnIndex，orderType）# 单元格排序  1.指定哪一列,2.升序or降序
+    # Qt.DescendingOrder    降序
+    # Qt.AscendingOrder     升序
+
+    setTextAlignment(Qt.AlignRight | Qt.AlignBottom)  # 分本对齐方式    Qt.AlignRight   Qt.AlignBottom
+
+    setSpan(行，列，合并多少行数，要合并的列数)  # 合并单元格，1表示不合并
+
+    tableWidget.setRowHeight(0, 80)     # 第1行的高度为80
+    tableWidget.setColumnWidth(2, 120)    # 设置cell的尺寸
+
+    newItem = QTableWidgetItem(QIcon('./images/bao1.png'),'背包')# 图片和文本添加到一个单元格中
+    setIconSize(QSize(width,height))    # 改变单元格图片的尺寸
+
+    # 在cell显示上下文菜单
+    self.tableWidget.setContextMenuPolicy(Qt.CustomContextMenu)     # 设置允许显示上下文菜单
+信号：self.tableWidget.customContextMenuRequested.connect(self.generateMenu)
+    def generateMenu(self,pos): # pos的相当于屏幕的坐标系
+        self.tableWidget.selectionModel().selection().indexes() # 获取所选择的行
+        menu = QMenu()  # 创建菜单项
+        item1 = menu.addAction("菜单项1")  # 添加菜单项
+        screenPos = self.tableWidget.mapToGlobal(pos)   #转为相对于菜单的
+        action = menu.exec(screenPos)   # 不点击就不会动
+
     tablewidget.setEditTriggers(QAbstractItemView.NoEditTriggers)   # 禁止编辑
     tablewidget.setSelectionBehavior(QAbstractItemView.SelectRows)  # 整行选择
 

8dd043d1be3b8e08c9ce1b520739cdd34c2c038d
wx <869154901@qq.com>
2020-04-15 16:17:47 +0800

    datetime

--- calendar.py
+++ calendar.py
@@ -1,3 +1,19 @@
+"""
+时间与日期
+dateTimeEdit1 = QDateTimeEdit()
+dateTimeEdit2 = QDateTimeEdit(QDateTime.currentDateTime())
+dateTimeEdit1.setMinimumDate(QDate.currentDate().addDays(-365))
+dateTimeEdit1.setMaximumDate(QDate.currentDate().addDays(365))
+
+    setDisplayFormat("yyyy/MM/dd HH-mm-ss")
+
+dateTimeEdit1.dateChanged.connect(self.onDateChanged)
+dateTimeEdit1.timeChanged.connect(self.onTimeChanged)
+dateTimeEdit1.dateTimeChanged.connect(self.onDateTimeChanged)
+
+datetime = self.dateTimeEdit1.dateTime()
+"""
+
 import sys
 from PyQt5.QtCore import Qt, QDate
 from PyQt5.QtGui import QPainter, QColor, QFont, QPen
